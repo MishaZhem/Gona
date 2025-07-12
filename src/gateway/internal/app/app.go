2baa8c3743843e20263ae0557ad933548d4fb705
MishaZhem <mz-100@mail.ru>
2025-07-13 00:28:44 +0300

    chore: changing structure of proto files (userservice and gateway)

---
+++ src/gateway/internal/app/app.go
@@ -0,0 +1,71 @@
+package app
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/MishaZhem/Gona/src/gateway/genproto/userpb"
+	"google.golang.org/grpc"
+)
+
+type App struct {
+	userClient userpb.UserServiceClient
+	timeout    time.Duration
+}
+
+func NewApp(conn *grpc.ClientConn, timeout time.Duration) *App {
+	return &App{
+		userClient: userpb.NewUserServiceClient(conn),
+		timeout:    timeout,
+	}
+}
+
+func (a *App) Register(username, email, password string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), a.timeout)
+	defer cancel()
+
+	_, err := a.userClient.Register(ctx, &userpb.RegisterRequest{
+		Username: username,
+		Email:    email,
+		Password: password,
+	})
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (a *App) Login(email, password string) (string, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), a.timeout)
+	defer cancel()
+
+	resp, err := a.userClient.Login(ctx, &userpb.LoginRequest{
+		Email:    email,
+		Password: password,
+	})
+	if err != nil {
+		return "", err
+	}
+	if resp.Error != "" {
+		return "", errors.New(resp.Error)
+	}
+	return resp.Token, nil
+}
+
+func (a *App) ValidateToken(token string) (string, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), a.timeout)
+	defer cancel()
+
+	resp, err := a.userClient.ValidateToken(ctx, &userpb.TokenRequest{
+		Token: token,
+	})
+	if err != nil {
+		return "", err
+	}
+	if resp.Error != "" {
+		return "", errors.New(resp.Error)
+	}
+	return resp.UserId, nil
+}
